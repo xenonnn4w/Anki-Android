921dec0e653424a937b3138ad0d99cdcbec7e0c3
Anoop <xenonoxidee@gmail.com>
2024-08-26 10:46:30 +0530

    Relocating the DeckPickerWidget-specific files to a dedicated package

--- AnkiDroid/src/test/java/com/ichi2/anki/widget/deckpickerwidget/DeckNameAndStatsTest.kt
+++ AnkiDroid/src/test/java/com/ichi2/anki/widget/deckpickerwidget/DeckNameAndStatsTest.kt
@@ -14,11 +14,11 @@
  *  this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.ichi2.anki.widget
+package com.ichi2.anki.widget.deckpickerwidget
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ichi2.anki.RobolectricTest
-import com.ichi2.widget.getDeckNameAndStats
+import com.ichi2.widget.deckpickerwidget.getDeckNameAndStats
 import org.junit.Test
 import org.junit.runner.RunWith
 import kotlin.test.assertEquals
